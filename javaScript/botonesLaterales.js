0e1c1a4d86619832b9bb86ecfb7337ce74c63461
braguilera <braaguileraa@gmail.com>
2024-11-22 14:11:39 -0300

    Funcionalidad de ocultar y mostrar botones

--- javaScript/botonesLaterales.js
+++ javaScript/botonesLaterales.js
@@ -1,22 +1,26 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const options = {
         root: null,
-        threshold: 0.4
+        threshold: 0.4 // El umbral del 20%
     };
 
-    const sectionSobreMi = document.getElementById('sobre_mi');
+    const sectionInicio = document.getElementById('inicio');
     const botonesLaterales = document.querySelector('.botones_laterales');
+    const inicioLinks = document.querySelector('.inicio_links');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                botonesLaterales.classList.add('mostrar');
-            } else {
+                // Cuando "Inicio" está visible en más del 20%
                 botonesLaterales.classList.remove('mostrar');
+                inicioLinks.classList.remove('ocultar');
+            } else {
+                // Cuando "Inicio" está menos visible que el 20%
+                botonesLaterales.classList.add('mostrar');
+                inicioLinks.classList.add('ocultar');
             }
         });
     }, options);
 
-    observer.observe(sectionSobreMi);
+    observer.observe(sectionInicio);
 });
