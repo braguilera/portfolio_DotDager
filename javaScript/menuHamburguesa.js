06473efdb4bd9a311653d89568e6d9482aaa5163
braguilera <braaguileraa@gmail.com>
2024-11-23 01:32:53 -0300

    Muchos estilos cambiados y media querys

--- javaScript/menuHamburguesa.js
+++ javaScript/menuHamburguesa.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
 
-    // Mostrar el menú al hacer clic en el botón abrir
     abrir.addEventListener("click", () => {
         nav.classList.add("visible");
     });
 
-    // Ocultar el menú al hacer clic en el botón cerrar
     cerrar.addEventListener("click", () => {
         nav.classList.remove("visible");
     });
